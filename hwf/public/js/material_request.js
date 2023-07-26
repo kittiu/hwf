8a7b624a224ef52346632d04292d12987e503118
Kitti U <kittiu@gmail.com>
2023-07-26 11:20:54 +0700

    IP

--- hwf/public/js/material_request.js
+++ hwf/public/js/material_request.js
@@ -1,1 +1,14 @@
 {% include "hwf/public/js/overrides/dimension_tree_filter.js" %}
+
+// Disable existing
+frappe.ui.form.off("Material Request Item", "qty") 
+frappe.ui.form.off("Material Request Item", "rate")
+
+frappe.ui.form.on("Material Request Item", {
+	qty: function (frm, doctype, name) {
+		const item = locals[doctype][name];
+		if (flt(item.qty) < flt(item.min_order_qty)) {
+			frappe.msgprint(__("Warning: Material Requested Qty is less than Minimum Order Qty"));
+		}
+	}
+})
